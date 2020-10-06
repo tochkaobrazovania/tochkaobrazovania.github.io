9af22ad1d662593d7effe4672f43a5d8e1dfbc8b
tochkaobrazovania <72462164+tochkaobrazovania@users.noreply.github.com>
2020-10-06 22:28:52 +0500

    Add files via upload

---
+++ precache-manifest.55ee2be919096427471d146cf56a9c8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae66c4bedabfc399ad73ee8039b82dab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80bfd4c8e5831a3fbe62",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "80bfd4c8e5831a3fbe62",
+    "url": "/static/js/2.c8a32a86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c8a32a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "444d013f1f352cacd744",
+    "url": "/static/js/main.52edc680.chunk.js"
+  },
+  {
+    "revision": "0266ed20f0bde903c660",
+    "url": "/static/js/runtime-main.61f118ae.js"
+  },
+  {
+    "revision": "beb9d6d4efadcc51342cedb966739fc3",
+    "url": "/static/media/covidimg.beb9d6d4.svg"
+  }
+]);
